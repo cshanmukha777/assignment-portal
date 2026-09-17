caec5f6d6804bf9e62803ee6ae936397043bf4b5
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-09-17 12:51:08 +0530

    Add POST assignments route

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+// Test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Assignment Portal API is running"
+    });
+});
+app.post("/assignments", async (req, res) => {
+    try {
+        const { title, deadline } = req.body;
+
+        const result = await pool.query(
+            `INSERT INTO assignments (title, deadline)
+             VALUES ($1, $2)
+             RETURNING *`,
+            [title, deadline]
+        );
+
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
